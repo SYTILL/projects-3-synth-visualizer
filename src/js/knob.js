0ca4614eaa6a466b00044041547c14f536940d52
J Yoon <41632499+SYTILL@users.noreply.github.com>
2024-03-15 15:03:15 +0900

    asdf

--- src/js/knob.js
+++ src/js/knob.js
@@ -2,97 +2,97 @@ let app = new Vue({
     el: '#app',
     data: {
         knobs:
-            {
+        {
 
-                10: {
-                    label: 'Unison',
-                    osc: "A", knobType: "updown", row: "upper",
-                    value: { real: 1.0, cur: 1, low: 1, high: 16 },
-                    color: '#0060df',
-                    active: true,
-                    selected: false,
-                },
-                11: {
-                    label: 'Detune',
-                    osc: "A", knobType: "circle", row: "upper",
-                    rotation: -132,
-                    value: { cur: 2, low: 0, high: 48 },
-                    color: '#0060df',
-                    active: true,
-                    selected: false,
-                },
-                12: {
-                    label: 'Blend',
-                    osc: "A", knobType: "circle", row: "upper",
-                    rotation: -132,
-                    value: { cur: 1, low: 1, high: 16 },
-                    color: '#0060df',
-                    active: true, selected: false,
-                },
-                13: {
-                    label: 'Pitch',
-                    osc: "A", knobType: "circle", row: "upper",
-                    rotation: -132,
-                    value: { cur: 1, low: 1, high: 16 },
-                    color: '#0060df',
-                    active: true, selected: false,
-                },
-                14: {
-                    label: 'Volume',
-                    osc: "A", knobType: "circle", row: "lower",
-                    rotation: 0,
-                    value: { cur: 0, low: -80, high: 80 },
-                    color: '#0060df',
-                    active: true, selected: false,
-                },
-
-
-                0: {
-                    label: 'Unison',
-                    osc: "B", knobType: "updown", row: "upper",
-                    value: { real: 1.0, cur: 1, low: 1, high: 16 },
-                    color: '#0060df',
-                    active: true,
-                    selected: false,
-                },
-                1: {
-                    label: 'Detune',
-                    osc: "B", knobType: "circle", row: "upper",
-                    rotation: -132,
-                    value: { cur: 2, low: 0, high: 48 },
-                    color: '#0060df',
-                    active: true,
-                    selected: false,
-                },
-                2: {
-                    label: 'Blend',
-                    osc: "B", knobType: "circle", row: "upper",
-                    rotation: -132,
-                    value: { cur: 1, low: 1, high: 16 },
-                    color: '#0060df',
-                    active: true, selected: false,
-                },
-                3: {
-                    label: 'Pitch',
-                    osc: "B", knobType: "circle", row: "upper",
-                    rotation: -132,
-                    value: { cur: 1, low: 1, high: 16 },
-                    color: '#0060df',
-                    active: true, selected: false,
-                },
-                4: {
-                    label: 'Volume',
-                    osc: "B", knobType: "circle", row: "lower",
-                    rotation: 0,
-                    value: { cur: 0, low: -80, high: 80 },
-                    color: '#0060df',
-                    active: true, selected: false,
-                },
+            10: {
+                label: 'Unison',
+                osc: "A", knobType: "updown", row: "upper",
+                value: { real: 1.0, cur: 1, low: 1, high: 16 },
+                color: '#0060df',
+                active: true,
+                selected: false,
             },
+            11: {
+                label: 'Detune',
+                osc: "A", knobType: "circle", row: "upper",
+                rotation: -132,
+                value: { cur: 2, low: 0, high: 48 },
+                color: '#0060df',
+                active: true,
+                selected: false,
+            },
+            12: {
+                label: 'Blend',
+                osc: "A", knobType: "circle", row: "upper",
+                rotation: 0,
+                value: { real: 50, cur: 0.5, low: 0, high: 100, ratio: 100 },
+                color: '#0060df',
+                active: true, selected: false,
+            },
+            13: {
+                label: 'Pitch',
+                osc: "A", knobType: "circle", row: "upper",
+                rotation: 0,
+                value: { real: 0, cur: 0, low: -240, high: 240, ratio: 10},
+                color: '#0060df',
+                active: true, selected: false,
+            },
+            14: {
+                label: 'Volume',
+                osc: "A", knobType: "circle", row: "lower",
+                rotation: 0,
+                value: { cur: 0, low: -80, high: 80 },
+                color: '#0060df',
+                active: true, selected: false,
+            },
+
+
+            0: {
+                label: 'Unison',
+                osc: "B", knobType: "updown", row: "upper",
+                value: { real: 1.0, cur: 1, low: 1, high: 16 },
+                color: '#0060df',
+                active: true,
+                selected: false,
+            },
+            1: {
+                label: 'Detune',
+                osc: "B", knobType: "circle", row: "upper",
+                rotation: -132,
+                value: { cur: 2, low: 0, high: 48 },
+                color: '#0060df',
+                active: true,
+                selected: false,
+            },
+            2: {
+                label: 'Blend',
+                osc: "B", knobType: "circle", row: "upper",
+                rotation: 0,
+                value: { real: 50, cur: 0.5, low: 0, high: 100, ratio: 100 },
+                color: '#0060df',
+                active: true, selected: false,
+            },
+            3: {
+                label: 'Pitch',
+                osc: "B", knobType: "circle", row: "upper",
+                rotation: 0,
+                value: { real: 0, cur: 0, low: -240, high: 240, ratio: 10},
+                color: '#0060df',
+                active: true, selected: false,
+            },
+            4: {
+                label: 'Volume',
+                osc: "B", knobType: "circle", row: "lower",
+                rotation: 0,
+                value: { cur: 0, low: -80, high: 80 },
+                color: '#0060df',
+                active: true, selected: false,
+            },
+        },
         currentY: 0,
-        mousemoveFunction: function (e) {
+        mousemoveFunction: async function (e) {
             let selectedKnob = Object.entries(app.knobs).filter(function (i) { return i[1].selected === true; });
-            
+
             if (selectedKnob.length > 0) {
                 selectedKnob = selectedKnob[0][1];
                 if (selectedKnob.knobType == "updown") {
@@ -112,13 +112,35 @@ let app = new Vue({
                     let range = selectedKnob.value.high - selectedKnob.value.low;
                     let valueRange = 264; // 132 (max rotation) - (-132) (min rotation)
                     let rotationRatio = (selectedKnob.rotation + 132) / valueRange; // Normalize rotation to range [0, 1]
-                    selectedKnob.value.cur = Math.round(selectedKnob.value.low + rotationRatio * range);
 
-                    // Ensure value.cur stays within bounds
-                    if (selectedKnob.value.cur > selectedKnob.value.high) {
-                        selectedKnob.value.cur = selectedKnob.value.high;
-                    } else if (selectedKnob.value.cur < selectedKnob.value.low) {
-                        selectedKnob.value.cur = selectedKnob.value.low;
+                    if (['Blend','Pitch'].includes(selectedKnob.label)) {
+                        selectedKnob.value.real = selectedKnob.value.low + rotationRatio * range;
+
+                        if (selectedKnob.value.real > selectedKnob.value.high) {
+                            selectedKnob.value.real = selectedKnob.value.high;
+                        } else if (selectedKnob.value.real < selectedKnob.value.low) {
+                            selectedKnob.value.real = selectedKnob.value.low;
+                        }
+
+                        selectedKnob.value.cur = (selectedKnob.value.real / selectedKnob.value.ratio).toFixed(2);
+                    }
+                    else {
+                        selectedKnob.value.cur = Math.round(selectedKnob.value.low + rotationRatio * range);
+
+                        if (selectedKnob.value.cur > selectedKnob.value.high) {
+                            selectedKnob.value.cur = selectedKnob.value.high;
+                        } else if (selectedKnob.value.cur < selectedKnob.value.low) {
+                            selectedKnob.value.cur = selectedKnob.value.low;
+                        }
+                    }
+                }
+
+                if (['Blend', 'Detune', 'Unison'].includes(selectedKnob.label)) {
+                    if (selectedKnob.osc == 'A') {
+                        await drawWaveform(oscA, 'A');
+                    }
+                    else if (selectedKnob.osc == 'B') {
+                        await drawWaveform(oscB, 'B');
                     }
                 }
             }
@@ -135,8 +157,12 @@ let app = new Vue({
         },
     },
     methods: {
-        unselectKnobs: function () {
-            for (var i in this.knobs) { this.knobs[i].selected = false; }
+        unselectKnobs: async function () {
+            for (var i in this.knobs) {
+                this.knobs[i].selected = false;
+            }
+            await drawWaveform(oscA);
+            await drawWaveform(oscB);
         }
     },
     mounted() {
